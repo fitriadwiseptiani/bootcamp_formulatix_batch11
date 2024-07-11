d75a5e730bb46a5db0f8083834f3e3023003189f
fitriadwiseptiani <fitriadwiseptiani11@gmail.com>
2024-07-11 08:07:16 +0700

    day3 upload

---
+++ Day3/inheritance/InheritanceComposition/Program.cs
@@ -0,0 +1,36 @@
+﻿
+class AirConditioner
+
+{
+	public Kondensor kondensor;
+	public AirConditioner(Kondensor kondensor)
+	{
+		this.kondensor = kondensor;
+	}
+	
+}
+class Kondensor
+{
+	public string jenis;
+}
+class Kompresor : Kondensor 
+{
+	
+}
+// class Blower : Kondensor
+// {
+	
+// }
+
+class Program
+{
+	static void Main()
+	{
+		Kondensor kondensor = new Kondensor();
+		AirConditioner airConditioner = new AirConditioner(kondensor);
+		
+		Kompresor kompresor= new Kompresor();
+		AirConditioner airConditioner2 = new AirConditioner(kompresor);
+	}
+	
+}
