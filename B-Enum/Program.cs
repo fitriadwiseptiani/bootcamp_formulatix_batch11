cc9fe9506340da8ceaa41190a3b16aa58b6f9e5f
fitriadwiseptiani <fitriadwiseptiani11@gmail.com>
2024-07-18 12:12:02 +0700

    Day9 upload

---
+++ B-Enum/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+	static void Main()
+	{
+		StatusCheker stat = new();
+		// Ada kemungkinan bisa typo
+		// string status = "NotFound";
+		Status status= Status.NotFound;
+		stat.Check(status);
+	}
+}
+
+// Enum dibuat agar tidak ada kesalahan saat pemanggilan atau penulisan suatu value
+// bisa digunakan untuk value yang paten
+public enum Status
+{
+	NotFound,
+	Redirected,
+	
+	WrongPassword,
+	Qwertyuiop
+}
+
+class StatusCheker
+{
+	public void Check(Status status)
+	{
+		// Bila dibuat seperti ini ada kemungkinan input data salah tulis typo
+		// if (Status == "NotFound")
+		// {
+		// 	Console.WriteLine("Error : Not Found");
+		// }
+		
+		if (status == Status.NotFound)
+		{
+			Console.WriteLine("Error : Not Found");
+		}
+		else if (status == Status.Redirected)
+		{
+			Console.WriteLine("Error : Redirected");
+		}
+		else if (status == Status.WrongPassword)
+		{
+			Console.WriteLine("Error : Password Salah");
+		}
+		else if (status == Status.Qwertyuiop)
+		{
+			Console.WriteLine("Error : Qwertuiop");
+		}
+		else
+		{
+			Console.WriteLine("Ga ada bisa coba lagi");
+		}
+	}
+}
---
+++ D-Enum3/Program.cs
@@ -0,0 +1,28 @@
+﻿class Piece {
+	public readonly PieceType pieceType;
+	public readonly Colour colour;
+}
+
+// Contoh untuk catur
+enum PieceType {
+	Pawn,
+	Rook,
+	King,
+	Queen,
+	Bishop,
+	Knight
+}
+
+// Contoh untuk Othello
+enum Colour {
+	White,
+	Black
+}
+
+// dibuat enum karena warna atau piecenya paten tidak berubah
+
+class Program
+{
+	Piece piece = new();
+	Colour check = Colour.Black;
+}
