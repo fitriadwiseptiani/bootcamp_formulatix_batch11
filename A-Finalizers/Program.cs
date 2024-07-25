c94eb4a5d0eb09a108278cc1d1307cc6ef8f8f34
fitriadwiseptiani <fitriadwiseptiani11@gmail.com>
2024-07-25 14:53:45 +0700

    Day 14 Uploaded

---
+++ A-Finalizers/Program.cs
@@ -0,0 +1,25 @@
+﻿class MyDestructor
+{
+	public MyDestructor ()
+	{
+		Console.WriteLine($"Program {GC.GetGeneration(this)} telah dibuat");
+	}
+	~MyDestructor ()
+	{
+		Console.WriteLine($"Program {GC.GetGeneration(this)} telah dihancurkan");
+	}
+	
+}
+class Program
+{
+	static void Main()
+	{
+		InstanceCreator();
+		GC.Collect();
+		// GC.Collect();
+	}
+	static void InstanceCreator()
+	{
+		MyDestructor myDestructor = new();
+	}
+}
---
+++ G-Dispose2/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+	static void Main()
+	{
+		MyMethod();
+	}
+	static void MyMethod()
+	{
+		FileStream fs = new();
+		AnotherMethod(fs);
+		fs.Dispose();
+	}
+	static void AnotherMethod(FileStream fs)
+	{
+		
+	}
+}
+
+//tidak bisa release unmanaged resource, karena belum tahu kondisi yang ada di method AnotherMethod
