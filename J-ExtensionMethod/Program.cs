75b5df75f5e090d429e68f9f3dccd63e9e54ba43
fitriadwiseptiani <fitriadwiseptiani11@gmail.com>
2024-07-26 09:40:04 +0700

    add extension method

---
+++ J-ExtensionMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿// class Program
+// {
+// 	static void Main()
+// 	{
+// 		string a = "Hello";
+// 		a.Cetak();  //tidak bisa dijalankan karena tidak ada method dari kelas tersebut yang dipanggil
+// 	}
+// }
+
+class Program
+{
+	static void Main()
+	{
+		string a = "Hello everybody";
+		a.Tampilkan();
+		int ax = 0;
+		ax.Tampilkan();
+		double[] myDouble = {3.0, 4.0, 9.0};
+		myDouble.Tampilkan();
+	}
+}
+public static class MyExtensions
+{
+	public static void Tampilkan(this object value)
+	{
+		Console.WriteLine(value);
+	}
+}
+
+// ditambahkan extension untuk membuat method yang sesuai dengan yang dibutuhkan
