4a5a87e2bd63d7ceb4ff8c30aebd148cefb6c25e
fitriadwiseptiani <fitriadwiseptiani11@gmail.com>
2024-07-30 14:59:45 +0700

    Add day 17 uploaded

---
+++ 2_AsyncTask/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+	static async Task Main()
+	{
+		Console.WriteLine("Program Starting");
+		Task<int> t1 = Task.Run(() => Add(3,19));
+		Task<int> t2 = Task.Run(() => CheckArray(2));
+		
+		await Task.WhenAll(t1, t2);
+		Console.WriteLine(t1.Result);
+		Console.WriteLine("Program Finished");
+	}
+	static async Task<int> CheckArray(int angka)
+	{
+		Console.WriteLine("Proses cek array akan dimulai");
+		Thread.Sleep(5000);
+		int[]myArray = {1,2,3,4,5,6};
+		try
+		{
+			await Task.Delay(1000);
+			Console.WriteLine("Nilai array adalah " + myArray[2] + " dan terdapat tambahan input angka " + angka);
+		}
+		catch(Exception e)
+		{
+			Console.WriteLine("Maaf ada kesalahan");
+			Console.WriteLine(e.Message);
+		}
+		return myArray[0];
+	}
+	static async Task<int> Add(int myArray, int angka) 
+	{
+		return myArray + angka;
+	}
+}
--- U_CancellationToken/Program.cs
+++ U_CancellationToken/Program.cs
@@ -1,26 +1,27 @@
 ﻿class Program
 {
-    static void Main()
-    {
-        Console.WriteLine("Program berjalan, memantau gempa...");
+	static void Main()
+	{
+		Console.WriteLine("Program berjalan, memantau gempa...");
 		CancellationTokenSource alarmCts = new CancellationTokenSource();
-        Task t2 = Task.Run(() => MonitorGempa(alarmCts.Token));
+		Task t2 = Task.Run(() => MonitorGempa(alarmCts.Token));
 		Task t1 = Task.Run(() =>
-        {
-           	Thread.Sleep(5000);
-            alarmCts.Cancel(); 
-        });
+		{
+		   	Thread.Sleep(5000);
+			alarmCts.Cancel(); 
+			
+		});
 		Task.WaitAny(t1,t2);
-        Console.ReadLine(); 
-    }
+		Console.ReadLine(); 
+	}
 
-    static void MonitorGempa(CancellationToken ct)
-    {
-        while (!ct.IsCancellationRequested)
-        {
-            Console.WriteLine("Monitoring gempa");
-            Thread.Sleep(1000); 
-        }
-        Console.WriteLine("Gempa nih...");
-    }
+	static void MonitorGempa(CancellationToken ct)
+	{
+		while (!ct.IsCancellationRequested)
+		{
+			Console.WriteLine("Monitoring gempa");
+			Thread.Sleep(1000); 
+		}
+		Console.WriteLine("Gempa nih...");
+	}
 }
